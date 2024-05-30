5db2ddea41ac64f18f64933b4004032cf01a3b22
Thomas Dubiel <td1729@hotmail.com>
2024-05-29 21:58:25 -0500

    new post resource functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  root "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "posts", to: "posts#index" 
+  get "posts/new", to: "posts#new", as: "new_post" 
+  get "posts/:id", to: "posts#show", as: "post" 
+  
 
-  get "/posts", to: "posts#index" 
-  get "posts/:id", to: "posts#show", as: "post"
+  #resources :posts
 end
